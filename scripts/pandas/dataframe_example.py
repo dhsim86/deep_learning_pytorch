f1d1a4465d6598aa8777606ea03d3b0d39de8203
Dongho Sim <dhsim86@gmail.com>
2025-08-09 17:12:19 +0900

    feat(dataframe): ✨ DataFrame 생성 및 조회 예제 추가
    - 다양한 자료형으로부터 DataFrame을 생성하는 방법을 추가했습니다.
    - CSV 파일로부터 DataFrame을 생성하는 예제를 포함했습니다.

--- scripts/pandas/dataframe_example.py
+++ scripts/pandas/dataframe_example.py
@@ -22,39 +22,9 @@ if __name__ == "__main__":
     print(f"DataFrame의 열: {df.columns}")
     print(f"DataFrame의 값:\n{df.values}")
 
-    # DataFrame 생성
-    # List, Series, dict, Numpy의 ndarray 등 다양한 자료형으로부터 생성 가능
-    # List로부터 생성
-    data = [
-        ["1000", "Steve", 90.72],
-        ["1001", "James", 78.09],
-        ["1002", "Doyeon", 98.43],
-        ["1003", "Jane", 64.19],
-        ["1004", "Pilwoong", 81.30],
-        ["1005", "Tony", 99.14],
-    ]
-    df = pd.DataFrame(data)  # 행과 열 인덱스는 0부터 시작하는 숫자로 생성됨
-    print(df)
+    # DataFrame의 특정 열 선택
+    print(f"A 열 선택 :\n{df['A']}")  # 열 'A' 선택
 
-    df = pd.DataFrame(data, columns=["학번", "이름", "점수"])  # 열 이름 지정
-    print(df)
-
-    # Dict로부터 생성
-    data = {
-        "학번": ["1000", "1001", "1002", "1003", "1004", "1005"],
-        "이름": ["Steve", "James", "Doyeon", "Jane", "Pilwoong", "Tony"],
-        "점수": [90.72, 78.09, 98.43, 64.19, 81.30, 99.14],
-    }
-    df = pd.DataFrame(data)  # 열 이름은 dict의 키로 지정됨
-    print(df)
-
-    # DataFrame 조회
-    print(df.head(3))  # 처음 3개 행 조회
-    print(df.tail(3))  # 마지막 3개 행 조회
-    print(df["학번"])  # 특정 열 ('학번') 조회
-
-    # 외부 데이터 (csv, jsonl 등)로부터 DataFrame 생성
-    # CSV 파일로부터 DataFrame 생성
-    df = pd.read_csv("scripts/pandas/example.csv")  # CSV 파일 경로 지정
-    print(df)
-    print(df.index)  # 인덱스는 자동으로 부여됨 (0부터 시작)
+    # DataFrame의 특정 행 선택
+    print(f"'one' 행 선택 :\n{df.loc['one']}")  # 인덱스 'one' 선택
+    print(f"두 번째 행 선택 :\n{df.iloc[1]}")  # 인덱스 위치 1 선택
