72ee30ee79cbd0306d04ad76b019a382e0bab25a
Dongho Sim <dhsim86@gmail.com>
2026-01-24 16:46:47 +0900

    refactor(softmax_regression): 🔧 nll_loss 함수 사용에 대한 주석 수정
    - 'nill_loss'를 'nil_loss'로 수정하여 정확한 용어 사용
    - 주석 내용의 가독성을 개선

--- softmax_regression/test.py
+++ softmax_regression/test.py
@@ -56,10 +56,10 @@ print("log_softmax 함수 결과:\n", log_softmax_result)
 cost = -(y_one_hot * torch.log(result)).sum(dim=1).mean()  # 크로스 엔트로피 비용함수 계산
 print("크로스 엔트로피 비용함수 결과:", cost.item())
 
-### nill_loss() 함수 사용
-#### nill_loss는 Negative Log Likelihood Loss의 약자
-#### nill_loss를 활용하면 원핫벡터를 쓸 필요없이 바로 실제값을 사용 가능
-#### nill_loss는 원래 비용함수 수식에서 F.log_softmax를 뺀 나머지 수식을 자동으로 수행
+### nil_loss() 함수 사용
+#### nil는 Negative Log Likelihood의 약자
+#### nil_loss를 활용하면 원핫벡터를 쓸 필요없이 바로 실제값을 사용 가능
+#### nil_loss는 원래 비용함수 수식에서 F.log_softmax를 뺀 나머지 수식을 자동으로 수행
 ####   -> y를 원핫벡터로 변경, 샘플별 비용합, 각 샘플별 비용 평균 구하기 
 nll_loss_cost = F.nll_loss(F.log_softmax(z, dim=1), y)
 print("nll_loss 함수 사용 비용함수 결과:", nll_loss_cost.item())
