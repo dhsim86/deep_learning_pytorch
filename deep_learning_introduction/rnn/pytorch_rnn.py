89d9268bf208a8ce3ed1384b41f3be78d031c59a
심동호 <dongho.sim@navercorp.com>
2026-06-25 15:15:52 +0900

    feat(rnn): add pytorch and numpy rnn implementation examples

---
+++ deep_learning_introduction/rnn/pytorch_rnn.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+input_size = 5 # 입력의 크기, 매 시점마다 들어가는 입력 크기
+hidden_size = 8 # 은닉 상태의 크기, RNN의 하이퍼파라미터
+
+# 입력 텐서 정의 (배치 크기 x 시점의 수 x 매 시점마다 들어가는 입력 크기)
+inputs = torch.Tensor(1, 10, 5)
+
+# RNN 셀 생성
+## 시점마다의 입력 크기 및 은닉 상태 크기 지정
+## batch_first를 통해 입력 텐선의 첫번째 차원이 배치 크기임을 알림
+cell = nn.RNN(input_size, hidden_size, batch_first=True)
+
+## 출력 확인
+### 첫 번째 리턴값: 모든 시점(timesteps)의 은닉 상태
+### 두 번째 리턴값: 마지막 시점의 은닉 상태
+outputs, _status = cell(inputs)
+
+### 10번의 시점동안 8차원의 은닉 상태가 출력됨
+print(outputs.shape) # torch.Size([1, 10, 8])
+
+### 최종 time-step의 hidden_state
+print(_status.shape) # torch.Size([1, 1, 8])
