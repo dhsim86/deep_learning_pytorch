1cc0bd668641a1d6a45132c3586743638bef783d
심동호 <dongho.sim@navercorp.com>
2026-08-24 09:26:44 +0900

    docs(gpt): add output examples and labels to gpt-2 example script

--- GPT/gpt_2_example.py
+++ GPT/gpt_2_example.py
@@ -6,9 +6,14 @@ model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 # 텍스트 인코딩 및 생성
 input_ids = tokenizer.encode("Hello, what's your name?", return_tensors='pt')
-
-# I'm a guy who's been living in the city for a while. I'm a guy who's
 output = model.generate(input_ids, max_length=30)
 
-print(output)
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+# output:  tensor([[15496,    11,   644,   338,   534,  1438,    30,   198,   198,    40,
+#          1101,   257,  3516,   508,   338,   587,  2877,   287,   262,  1748,
+#           329,   257,   981,    13,   314,  1101,   257,  3516,   508,   338]])
+print("output: ", output)
+
+# decoded output:  Hello, what's your name?
+#
+# I'm a guy who's been living in the city for a while. I'm a guy who's
+print("decoded output: ", tokenizer.decode(output[0], skip_special_tokens=True))
